1d2dc6dc2fdeb8f4cae0a7e8f0712eca039e9f32
KimByeongKou <anapple63@gmail.com>
2016-03-28 12:37:17 +0900

    set1

---
+++ src/test/java/org/eney/test/MySQLConnectionTest.java
@@ -0,0 +1,34 @@
+package org.eney.test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class MySQLConnectionTest {
+
+	private static final String DRIVER = 
+			"net.sf.log4jdbc.sql.jdbcapi.DriverSpy";
+	private static final String URL = 
+			"jdbc:log4jdbc:mysql://127.0.0.1:3306/mcn";
+	private static final String USER = 
+			"root";
+	private static final String PW = 
+			"root";
+			
+	
+	@Test
+	public void testConnection() throws Exception{
+		
+		Class.forName(DRIVER);
+		
+		try(Connection con = DriverManager.getConnection(URL, USER, PW)){
+			
+			System.out.println(con);
+			
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
